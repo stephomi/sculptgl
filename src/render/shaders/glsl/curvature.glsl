92781efa826351cfe52a9be5b08ed8951cd0b4c6
stephomi <stephaneginier@gmail.com>
2015-03-21 03:53:13 +0100

    Adds signed curvature

--- src/render/shaders/glsl/curvature.glsl
+++ src/render/shaders/glsl/curvature.glsl
@@ -1,12 +1,26 @@
-
+// http://madebyevan.com/shaders/curvature/
 vec3 computeCurvature( const in vec3 vertex, const in vec3 normal, const in vec3 color) {
-  float curvature = length(fwidth(normal)) / length(vertex);
-  float shine = clamp(curvature * 200.0, 0.0, 1.0);
+  vec3 n = normalize(normal);
+  // Compute curvature
+  vec3 dx = dFdx(n);
+  vec3 dy = dFdy(n);
+  vec3 xneg = n - dx;
+  vec3 xpos = n + dx;
+  vec3 yneg = n - dy;
+  vec3 ypos = n + dy;
+  float depth = length(vertex);
+  float curvature = (cross(xneg, xpos).y - cross(yneg, ypos).x) * 4.0 / depth;
+
+  // Compute surface properties
+  float corrosion = clamp(-curvature * 15.0, 0.0, 1.0);
+  float shine = clamp(curvature * 25.0, 0.0, 1.0);
   vec3 light = normalize(vec3(0.0, 1.0, 10.0));
-  const vec3 ambient = vec3(0.15, 0.1, 0.1);
-  vec3 diffuse = mix(color, color*2.0, shine) - ambient;
-  vec3 spec = mix(vec3(0.0), vec3(1.0) - ambient - diffuse, shine);
-  const float shininess = 128.0;
-  float cosAngle = dot(normal, light);
-  return ambient + diffuse * max(0.0, cosAngle) + spec * pow(max(0.0, cosAngle), shininess);
+  vec3 ambient = vec3(0.15, 0.1, 0.1);
+  vec3 diffuse = mix(mix(color, color * 0.3, corrosion), color * 2.0, shine) - ambient;
+  vec3 specular = mix(vec3(0.0), vec3(1.0) - ambient - diffuse, shine);
+  float shininess = 128.0;
+
+  // Compute final color
+  float cosAngle = dot(n, light);
+  return ambient + diffuse * max(0.0, cosAngle) + specular * pow(max(0.0, cosAngle), shininess);
 }
