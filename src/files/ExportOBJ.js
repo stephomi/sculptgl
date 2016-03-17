d6c3ca3766f3b1da428b8ddbe7cbdd6090caa952
stephomi <stephaneginier@gmail.com>
2016-03-17 18:45:46 +0100

    Make the obj exporter more robust with big meshes

--- src/files/ExportOBJ.js
+++ src/files/ExportOBJ.js
@@ -18,16 +18,46 @@ define(function (require, exports, module) {
     }
     return new Blob([data]);
   };
+
+  var appendString = function (buffer, str, it) {
+    for (var k = 0, strLen = str.length; k < strLen; k++) {
+      buffer[it++] = str.charCodeAt(k);
+    }
+    return it;
+  };
+
   Export.addMesh = function (mesh, data, offsets, saveColor) {
     var vAr = mesh.getVertices();
     var cAr = mesh.getColors();
     var mAr = mesh.getMaterials();
     var fAr = mesh.getFaces();
+
     var nbVertices = mesh.getNbVertices();
     var nbFaces = mesh.getNbFaces();
+    var nbTexCoords = mesh.getNbTexCoords();
+
     var matrix = mesh.getMatrix();
     var i = 0;
     var j = 0;
+
+    var str = '';
+
+    // chrome uses too much resources when appending many times to a very big string
+    // Use of a buffer array as an intermediate storage to avoid crashes 
+
+    var estimaton = 0;
+    // 25 chars limits per float
+    var cpf = 20;
+    if (saveColor) estimaton = nbVertices * (8 + cpf * 6);
+    else estimaton = nbVertices * (5 + cpf * 3 + 8 + 6);
+    estimaton += nbTexCoords * (5 + cpf * 2) + nbFaces * (6 + 15 * 4);
+
+    var bufView = new Uint8Array(Math.max(1000, estimaton));
+    var it = 0;
+
+    ///////////
+    // VERTICES
+    ///////////
     var ver = [0.0, 0.0, 0.0];
     for (i = 0; i < nbVertices; ++i) {
       j = i * 3;
@@ -35,32 +65,42 @@ define(function (require, exports, module) {
       ver[1] = vAr[j + 1];
       ver[2] = vAr[j + 2];
       vec3.transformMat4(ver, ver, matrix);
-      data += 'v ' + ver[0] + ' ' + ver[1] + ' ' + ver[2];
-      data += (saveColor ? ' ' + cAr[j] + ' ' + cAr[j + 1] + ' ' + cAr[j + 2] + '\n' : '\n');
+
+      str = 'v ' + ver[0] + ' ' + ver[1] + ' ' + ver[2];
+      str += (saveColor ? ' ' + cAr[j] + ' ' + cAr[j + 1] + ' ' + cAr[j + 2] + '\n' : '\n');
+
+      it = appendString(bufView, str, it);
     }
+
+    ////////////////
+    // COLORS-zbrush
+    ////////////////
     if (!saveColor) {
       // zbrush-like vertex color
       var nbChunck = Math.ceil(nbVertices / 64);
       for (i = 0; i < nbChunck; ++i) {
-        data += '#MRGB ';
+        str = '#MRGB ';
         j = i * 64;
         var nbCol = j + (i === nbChunck - 1 ? nbVertices % 64 : 64);
         for (; j < nbCol; ++j) {
-          data += 'ff';
+          str += 'ff';
           var cId = j * 3;
           var r = Math.round(cAr[cId] * 255).toString(16);
           var g = Math.round(cAr[cId + 1] * 255).toString(16);
           var b = Math.round(cAr[cId + 2] * 255).toString(16);
-          data += r.length === 1 ? '0' + r : r;
-          data += g.length === 1 ? '0' + g : g;
-          data += b.length === 1 ? '0' + b : b;
+          str += r.length === 1 ? '0' + r : r;
+          str += g.length === 1 ? '0' + g : g;
+          str += b.length === 1 ? '0' + b : b;
         }
-        data += '\n';
+        str += '\n';
+
+        it = appendString(bufView, str, it);
       }
+
       // zbrush-like vertex material
       nbChunck = Math.ceil(nbVertices / 46);
       for (i = 0; i < nbChunck; ++i) {
-        data += '#MAT ';
+        str = '#MAT ';
         j = i * 46;
         var nbMat = j + (i === nbChunck - 1 ? nbVertices % 46 : 46);
         for (; j < nbMat; ++j) {
@@ -68,21 +108,33 @@ define(function (require, exports, module) {
           var ro = Math.round(mAr[mId] * 255).toString(16);
           var m = Math.round(mAr[mId + 1] * 255).toString(16);
           var a = Math.round(mAr[mId + 2] * 255).toString(16);
-          data += ro.length === 1 ? '0' + ro : ro;
-          data += m.length === 1 ? '0' + m : m;
-          data += a.length === 1 ? '0' + a : a;
+          str += ro.length === 1 ? '0' + ro : ro;
+          str += m.length === 1 ? '0' + m : m;
+          str += a.length === 1 ? '0' + a : a;
         }
-        data += '\n';
+        str += '\n';
+
+        it = appendString(bufView, str, it);
       }
     }
-    var nbTexCoords = mesh.getNbTexCoords();
+
+    /////
+    // UV
+    /////
     var fArUV = mesh.getFacesTexCoord();
     var uvAr = mesh.getTexCoords();
     var saveUV = mesh.hasUV();
-    for (i = 0; i < nbTexCoords; ++i) {
-      j = i * 2;
-      data += 'vt ' + uvAr[j] + ' ' + uvAr[j + 1] + '\n';
+    if (saveUV) {
+      for (i = 0; i < nbTexCoords; ++i) {
+        j = i * 2;
+        str = 'vt ' + uvAr[j] + ' ' + uvAr[j + 1] + '\n';
+        it = appendString(bufView, str, it);
+      }
     }
+
+    ////////
+    // FACES
+    ////////
     var offV = offsets[0];
     var offTex = offsets[1];
     offsets[0] += nbVertices;
@@ -91,17 +143,29 @@ define(function (require, exports, module) {
       j = i * 4;
       var id = fAr[j + 3];
       if (saveUV) {
-        data += 'f ' + (offV + fAr[j]) + '/' + (offTex + fArUV[j]);
-        data += ' ' + (offV + fAr[j + 1]) + '/' + (offTex + fArUV[j + 1]);
-        data += ' ' + (offV + fAr[j + 2]) + '/' + (offTex + fArUV[j + 2]);
-        data += (id >= 0 ? ' ' + (offV + id) + '/' + (offTex + fArUV[j + 3]) + '\n' : '\n');
+        str = 'f ' + (offV + fAr[j]) + '/' + (offTex + fArUV[j]);
+        str += ' ' + (offV + fAr[j + 1]) + '/' + (offTex + fArUV[j + 1]);
+        str += ' ' + (offV + fAr[j + 2]) + '/' + (offTex + fArUV[j + 2]);
+        str += (id >= 0 ? ' ' + (offV + id) + '/' + (offTex + fArUV[j + 3]) + '\n' : '\n');
       } else {
-        data += 'f ' + (offV + fAr[j]);
-        data += ' ' + (offV + fAr[j + 1]);
-        data += ' ' + (offV + fAr[j + 2]);
-        data += (id >= 0 ? ' ' + (offV + id) + '\n' : '\n');
+        str = 'f ' + (offV + fAr[j]);
+        str += ' ' + (offV + fAr[j + 1]);
+        str += ' ' + (offV + fAr[j + 2]);
+        str += (id >= 0 ? ' ' + (offV + id) + '\n' : '\n');
       }
+      it = appendString(bufView, str, it);
     }
+
+    bufView = bufView.subarray(0, it);
+
+    if (window.TextDecoder) {
+      var de = new TextDecoder();
+      return data + de.decode(bufView);
+    }
+
+    for (i = 0; i < it; ++i)
+      data += String.fromCharCode(bufView[i]);
+
     return data;
   };
 
