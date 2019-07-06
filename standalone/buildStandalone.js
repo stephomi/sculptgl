107ffd4d030ef18e66b4c51541526b9d4cb40d97
Stéphane GINIER <stephaneginier@gmail.com>
2019-07-06 02:25:07 +0200

    Builds only window standalone

--- standalone/buildStandalone.js
+++ standalone/buildStandalone.js
@@ -4,10 +4,10 @@ packager({
   dir: '.',
   out: '.',
   name: 'SculptGL',
-  all: true,
-  // arch: 'x64',
-  // platform: 'win32',
+  all: false,
+  arch: ['ia32', 'x64'],
+  platform: 'win32',
   asar: false,
   overwrite: true,
-  ignore:[/\b(buildStandalone)/]
+  ignore: [/\b(buildStandalone)/]
 }, function () {});
